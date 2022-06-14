142909324e545f3f08e1d1311d643d98af6e3186
RathiSabari <rathipriya16@gmail.com>
2022-06-14 22:13:24 +0800

    Developed Layout for my profile page

--- EMS/src/app/app-routing.module.ts
+++ EMS/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/login/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {path:'',pathMatch:'full',redirectTo:'login'},
  { path: 'login', component: LoginComponent },
- {path:'home',component:HomeComponent}
+ {path:'home',component:HomeComponent},
+ {path:'profile',component:ProfileComponent},
+ {path:'',pathMatch:'full',redirectTo:'login'},
 ];
 
 @NgModule({
